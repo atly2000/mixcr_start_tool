49db6e6f14874debaf3f2343bb44fc9776b8828b
atly2000 <87032440+atly2000@users.noreply.github.com>
2026-08-17 17:03:40 -0400

    Update make01.sh

--- templates/extra_scripts/make01.sh
+++ templates/extra_scripts/make01.sh
@@ -3,26 +3,30 @@
 #You can move this to the main working directory if you want to use it instead of 00_initialize. 
 module load python
 
-
 #Define Directories to Use
 NEW_PATIENT=$(<input/sample_names.txt)
 NEW_FASTQ=$(<input/fastq_dir.txt)
 NEW_OUT=$(<input/out_dir.txt)
 NEW_B=$(<input/b_dir.txt)
-
-
+NEW_STYLE_R1=$(<input/style_R1.txt)
+NEW_STYLE_R2=$(<input/style_R2.txt)
+          
 #Making MIXCR Analyze Scripts
 cp templates/01_TEMPLATE_mixcr_analyze_script_maker.py prep_scripts/01_mixcr_analyze_script_maker.py
 sed -i "s|PATIENT_LIST|$NEW_PATIENT|g" prep_scripts/01_mixcr_analyze_script_maker.py
 sed -i "s|FASTQ_LIST|$NEW_FASTQ|g" prep_scripts/01_mixcr_analyze_script_maker.py
 sed -i "s|OUT_LIST|$NEW_OUT|g" prep_scripts/01_mixcr_analyze_script_maker.py
+sed -i "s|STYLE_R1|$NEW_STYLE_R1|g" prep_scripts/01_mixcr_analyze_script_maker.py
+sed -i "s|STYLE_R2|$NEW_STYLE_R2|g" prep_scripts/01_mixcr_analyze_script_maker.py
 python prep_scripts/01_mixcr_analyze_script_maker.py > run_scripts/01_mixcr_run.swarm
-
+          
 #Making MIXCR Export Scripts
 cp templates/02_TEMPLATE_mixcr_export_script_maker.py prep_scripts/02_mixcr_script_maker.py
 sed -i "s|PATIENT_LIST|$NEW_PATIENT|g" prep_scripts/02_mixcr_script_maker.py
 sed -i "s|B_LIST|$NEW_B|g" prep_scripts/02_mixcr_script_maker.py
 sed -i "s|OUT_LIST|$NEW_OUT|g" prep_scripts/02_mixcr_script_maker.py
+sed -i "s|STYLE_R1|$NEW_STYLE_R1|g" prep_scripts/02_mixcr_script_maker.py
+sed -i "s|STYLE_R2|$NEW_STYLE_R2|g" prep_scripts/02_mixcr_script_maker.py
 python prep_scripts/02_mixcr_script_maker.py > run_scripts/02_mixcr_export.sh
 
 
